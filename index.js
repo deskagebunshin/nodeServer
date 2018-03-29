f9c96f3539ec1dda688ef18f8103e4196482290f
deskagebunshin <deskagebunshin@gmail.com>
2018-03-29 09:19:10 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require ('express');
+var app = express();
+var server = require('http').createServer(app);
+
+var io = require('socket.io').listen(server);
+var users = [];
+var connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('server running...');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html')
+});
+
+io.sockets.on('connection', function(socket){
+  connections.push(socket);
+  console.log('Conected: %s sockets connected', connections.length)
+
+  // Disconnect
+  socket.on('disconnect', function () {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected %s sockets connected', connections.length);
+  });
+
+  //
+  socket.on('send message', function (data) {
+    console.log(data);
+    io.sockets.emit('new message', {msg: data});
+  })
+});
